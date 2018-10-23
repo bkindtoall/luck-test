359b4cab505211088db6d10da05292e84c1360fb
Charles Robertson and Patricia Filiopovic <patricia@>
2018-10-23 11:28:59 -0700

    used replace -normal expression-

---
+++ puzzle/js/scripts.js
@@ -0,0 +1,12 @@
+var scripts = 'Believe you can and you are halfway there';
+
+// var newSentence = scripts.split("");
+
+
+
+// var vowels = ["a", "e", "i", "o", "u"]
+//
+// for (var index=0; index < vowels.length; index += 1) {
+  var updatedSentence = scripts.replace (/a|e|i|o|u/gi, "-");
+  alert (updatedSentence);
+// }
