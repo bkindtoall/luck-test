128ee1ae036e426d0ea34f9a7798c79982bfcb50
Charles Robertson and Patricia Filiopovic <charles@>
2018-10-23 09:56:27 -0700

    changed foreach to for

---
+++ forpractice/js/scripts.js
@@ -0,0 +1,9 @@
+/*var languages = ['HTML', 'CSS', 'JavaScript'];
+languages.forEach(function(language) {
+    alert('I love ' + language + '!');
+});*/
+
+var languages = ['HTML', 'CSS', 'Javascript'];
+for (var index = 0; index < languages.length; index += 1) {
+  alert('I love ' + languages[index] + '!');
+}
